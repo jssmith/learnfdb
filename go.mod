22c9fb47d4df0184746b09136202c218238e60ef
Johann Schleier-Smith <jssmith@gmail.com>
2020-08-14 13:30:25 -0700

    playing with foundationdb and go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jssmith/learnfdb
+
+go 1.14
+
+require (
+	github.com/apple/foundationdb/bindings/go v0.0.0-20200605000326-4f24dd7bd068
+	github.com/pkg/errors v0.9.1
+)
---
+++ hello.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/apple/foundationdb/bindings/go/src/fdb"
+)
+
+func setKeys(db fdb.Database, n, nKeys, size int) error {
+	start := time.Now()
+	buf := make([]byte, size)
+	for i := 0; i < n; i++ {
+		rand.Read(buf)
+		mykey := fmt.Sprintf("hello-%d", rand.Intn(nKeys))
+		_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
+			tr.Set(fdb.Key(mykey), buf)
+			return
+		})
+		if err != nil {
+			return errors.Wrap(err, "Unable to set FDB database")
+		}
+	}
+	elapsed := time.Now().Sub(start)
+	rate := float64(n) / elapsed.Seconds()
+	log.Printf("set completed %d iterations with rate %f", n, rate)
+	return nil
+}
+
+func getKeys(db fdb.Database, n, nKeys int) error {
+	start := time.Now()
+	for i := 0; i < n; i++ {
+		mykey := fmt.Sprintf("hello-%d", rand.Intn(nKeys))
+		_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
+			ret = tr.Get(fdb.Key(mykey)).MustGet()
+			return
+		})
+		if err != nil {
+			return errors.Wrap(err, "Unable to read FDB database value")
+		}
+	}
+	elapsed := time.Now().Sub(start)
+	rate := float64(n) / elapsed.Seconds()
+	log.Printf("get completed %d iterations with rate %f", n, rate)
+	return nil
+}
+
+func setKeysConcurrent(db fdb.Database, n, nKeys, size, nConcurrent int) error {
+	var wg sync.WaitGroup
+	start := time.Now()
+	wg.Add(nConcurrent)
+	completed := make([]int, nConcurrent)
+	for i := 0; i < nConcurrent; i++ {
+		go func(id int) {
+			defer wg.Done()
+			buf := make([]byte, size)
+			ct := 0
+			for i := 0; i < n; i++ {
+				rand.Read(buf)
+				mykey := fmt.Sprintf("hello-%d", rand.Intn(nKeys))
+				_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
+					tr.Set(fdb.Key(mykey), buf)
+					return
+				})
+				// if err != nil {
+				// 	return errors.Wrap(err, "Unable to set FDB database")
+				// }
+				if err != nil {
+					log.Printf("Error %v", err)
+					break
+				}
+				ct++
+			}
+			completed[id] = ct
+		}(i)
+	}
+	wg.Wait()
+	nComplete := 0
+	for _, n := range completed {
+		nComplete += n
+	}
+	elapsed := time.Now().Sub(start)
+	rate := float64(nComplete*nConcurrent) / elapsed.Seconds()
+	log.Printf("concurrent set completed %d iterations with rate %f", n, rate)
+	return nil
+}
+
+func sayHello(db fdb.Database) error {
+	_, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
+		tr.Set(fdb.Key("hello"), []byte("world"))
+		return
+	})
+	if err != nil {
+		return errors.Wrap(err, "Unable to set FDB database value")
+	}
+	ret, err := db.Transact(func(tr fdb.Transaction) (ret interface{}, e error) {
+		ret = tr.Get(fdb.Key("hello")).MustGet()
+		return
+	})
+	if err != nil {
+		return errors.Wrap(err, "Unable to read FDB database value")
+	}
+
+	v := ret.([]byte)
+	fmt.Printf("hello, %s\n", string(v))
+	return nil
+}
+
+func main() {
+	fdb.MustAPIVersion(620)
+	// Open the default database from the system cluster
+	db := fdb.MustOpenDefault()
+
+	err := sayHello(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = setKeys(db, 100, 1000, 1024)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = getKeys(db, 100, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, concurrency := range []int{16, 32} {
+		err = setKeysConcurrent(db, 1000, 1000, 1024, concurrency)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Database reads and writes happen inside transactions
+	// ret, e := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+	// 	tr.Set(fdb.Key("hello"), []byte("world"))
+	// 	return tr.Get(fdb.Key("foo")).MustGet(), nil
+	// 	// db.Transact automatically commits (and if necessary,
+	// 	// retries) the transaction
+	// })
+	// if e != nil {
+	// 	log.Fatalf("Unable to perform FDB transaction (%v)", e)
+	// }
+
+}
